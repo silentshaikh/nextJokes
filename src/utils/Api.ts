476b3eb9e1b68fdfd317f10149a40b1cf21bdb7c
silentshaikh <sheikhabdulmoiz283@gmail.com>
2024-11-10 23:33:11 +0500

    add const varriabl

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,1 +1,1 @@
-export let jokeApi:string = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single`;
+export const jokeApi:string = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single`;
